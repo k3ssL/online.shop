445b29c2397c329aa4eff2f95da7f3c8929bb7b5
k3ssL <viqwer3@yandex.ru>
2024-04-10 01:42:06 +0500

    create users model

---
+++ server/src/users/users.model.ts
@@ -0,0 +1,27 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
+
+interface UserCreationAttrs {
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+    export class User extends Model<User, UserCreationAttrs> {
+
+    @ApiProperty({example: '1', description: 'Unique user ID'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number
+
+    @ApiProperty({example: 'example@gmail.com', description: 'User mailing address'})
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string
+
+    @ApiProperty({example: 'password123', description: 'User password'})
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string
+
+    @ApiProperty({example: 'USER', description: 'User role'})
+    @Column({type: DataType.STRING, allowNull: false})
+    role: string
+}

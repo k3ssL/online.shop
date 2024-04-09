23511fd08f1cfecf04b8c5d0c995b5ac8e326047
k3ssL <viqwer3@yandex.ru>
2024-04-10 02:33:46 +0500

    create user method on controller level

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {UsersService} from "./users.service";
+import {User} from "./users.model";
+import {CreateUserDto} from "./dto/create-user.dto";
+
+@ApiTags('Users')
+@Controller('users')
+export class UsersController {
+    constructor(private userService: UsersService) {}
+
+    @ApiOperation({summary: 'Create user'})
+    @ApiResponse({status: 200, type: User})
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+
+    }
+}

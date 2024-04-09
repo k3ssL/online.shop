ee83f39958a041560f14a651d80e7c02e83cc970
k3ssL <viqwer3@yandex.ru>
2024-04-10 01:52:55 +0500

    connect database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import * as process from "process";
+import {User} from "./users/users.model";
+import {UsersModule} from "./users/users.module";
+import {ConfigModule} from "@nestjs/config";
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: 'postgres',
+      password: 'root',
+      database: process.env.POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}

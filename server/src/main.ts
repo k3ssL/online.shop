7b13b6880da50d05c42c5c3a94f288527b31beb8
k3ssL <viqwer3@yandex.ru>
2024-04-10 01:52:25 +0500

    start function

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+require('dotenv').config()
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  const PORT = process.env.PORT || 5000
+  const app  = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+      .setTitle('Online-shop API')
+      .setDescription('Documentation REST API')
+      .setVersion('1.0.0')
+      .addTag('k3ssL')
+      .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/api/docs', app, document)
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
-bootstrap();
+
+start()

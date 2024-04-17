c150b1187e5d3a7f4f6e44c1a3cf172a641246a3
k3ssL <viqwer3@yandex.ru>
2024-04-18 00:05:38 +0500

    add model device info, add relations

---
+++ server/src/deviceInfo/deviceInfo.model.ts
@@ -0,0 +1,22 @@
+import {BelongsTo, Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
+import {Device} from "../device/device.model";
+
+@Table({tableName: 'device_info'})
+export class DeviceInfo extends Model<DeviceInfo> {
+
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number
+
+    @ForeignKey(() => Device)
+    @Column({type: DataType.INTEGER, allowNull: false})
+    deviceId: number
+
+    @Column({type: DataType.INTEGER, allowNull: false})
+    title: string
+
+    @Column({type: DataType.STRING, allowNull: false})
+    description: string
+
+    @BelongsTo(() => Device)
+    device: Device
+}

c6f1f7e0b5a527c497926f589e8505d99307f8de
k3ssL <viqwer3@yandex.ru>
2024-04-18 00:08:11 +0500

    add linking model type brand, add relations

---
+++ server/src/typeBrand/typeBrand.module.ts
@@ -0,0 +1,16 @@
+import {Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
+import {Type} from "../type/type.model";
+import {Brand} from "../brand/brand.model";
+
+@Table({tableName: 'type_brand'})
+export class TypeBrand extends Model<TypeBrand> {
+
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number
+
+    @ForeignKey(() => Type)
+    type: Type
+
+    @ForeignKey(() => Brand)
+    brand: Brand
+}

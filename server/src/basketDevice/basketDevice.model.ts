a3d0f045bee9f0b015b4bfd17e2937cfafed9593
k3ssL <viqwer3@yandex.ru>
2024-04-18 00:08:42 +0500

    add relations

--- server/src/basketDevice/basketDevice.model.ts
+++ server/src/basketDevice/basketDevice.model.ts
@@ -1,6 +1,6 @@
 import {BelongsTo, Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
-import {User} from "../users/users.model";
 import {Basket} from "../basket/basket.model";
+import {Device} from "../device/device.model";
 
 @Table({tableName: 'basket_device'})
 export class BasketDevice extends Model<BasketDevice> {
@@ -8,17 +8,17 @@ export class BasketDevice extends Model<BasketDevice> {
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number
 
+    @ForeignKey(() => Device)
+    @Column({type: DataType.INTEGER, allowNull: false})
+    deviceId: number
+
     @ForeignKey(() => Basket)
     @Column({type: DataType.INTEGER, allowNull: false})
-    basketId: number
-
-    @ForeignKey(() => User)
-    @Column({type: DataType.INTEGER, allowNull: false})
-    userId: string
+    basketId: string
 
     @BelongsTo(() => Basket)
     basket: Basket
 
-    @BelongsTo(() => User)
-    user: User
+    @BelongsTo(() => Basket)
+    basketDevice: BasketDevice
 }

a005769207022d5e89cc2b2591848d1d8e38d0a2
k3ssL <viqwer3@yandex.ru>
2024-04-21 00:44:26 +0500

    add api property

--- server/src/type/type.model.ts
+++ server/src/type/type.model.ts
@@ -2,13 +2,16 @@ import {BelongsToMany, Column, DataType, HasMany, Model, Table} from "sequelize-
 import {Device} from "../device/device.model";
 import {Brand} from "../brand/brand.model";
 import {TypeBrand} from "../typeBrand/typeBrand.module";
+import {ApiProperty} from "@nestjs/swagger";
 
 @Table({tableName: 'type'})
 export class Type extends Model<Type> {
 
+    @ApiProperty({example: '1', description: 'Unique type ID'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number
 
+    @ApiProperty({example: 'Fridge', description: 'Product Type'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     name: string
 

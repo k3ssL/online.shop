d69d8df321e2f0c4ef04927ee0e502995024d2fd
k3ssL <viqwer3@yandex.ru>
2024-04-21 00:45:12 +0500

    add create type dto

---
+++ server/src/type/dto/create-type.dto.ts
@@ -0,0 +1,9 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {IsString} from "class-validator";
+
+export class CreateTypeDto {
+
+    @ApiProperty({example: 'Fridge', description: 'Product type'})
+    @IsString({message: 'Must be a string'})
+    readonly name: string
+}

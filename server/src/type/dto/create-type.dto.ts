8bc3277157457337550ce4cc8c7ffb4172dc7e93
k3ssL <viqwer3@yandex.ru>
2024-04-25 23:45:26 +0500

    refactoring

--- server/src/type/dto/create-type.dto.ts
+++ server/src/type/dto/create-type.dto.ts
@@ -2,7 +2,6 @@ import {ApiProperty} from "@nestjs/swagger";
 import {IsString} from "class-validator";
 
 export class CreateTypeDto {
-
     @ApiProperty({example: 'Fridge', description: 'Product type'})
     @IsString({message: 'Must be a string'})
     readonly name: string

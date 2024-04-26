d9df02e7dda250d5436542a9db9fd29884c76a7a
k3ssL <viqwer3@yandex.ru>
2024-04-26 22:06:51 +0500

    add storage for upload files

---
+++ server/src/device/storage.ts
@@ -0,0 +1,18 @@
+import { diskStorage } from "multer";
+import { extname } from "path";
+
+const generateId = () =>
+    Array(18)
+        .fill(null)
+        .map(() => Math.round(Math.random() * 16).toString(16))
+        .join('')
+
+const normalizeFileName = (req: any, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) => {
+    const fileExtName = extname(file.originalname)
+    callback(null, `${generateId()}${fileExtName}`)
+}
+
+export const fileStorage = diskStorage({
+    destination: 'src/static',
+    filename: normalizeFileName
+})

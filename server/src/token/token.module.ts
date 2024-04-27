006e58f8ebe78ed2f929c6a35b0a471870a3315c
k3ssL <viqwer3@yandex.ru>
2024-04-28 01:35:23 +0500

    add auth middleware

---
+++ server/src/token/token.module.ts
@@ -0,0 +1,7 @@
+import { Column, DataType, Model, } from "sequelize-typescript";
+import {ApiProperty} from "@nestjs/swagger";
+
+export class Token extends Model<Token> {
+    @ApiProperty({ example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJleGFtcGxlQGdtYWlsLmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzE0MjQ3NDk5LCJleHAiOjE3MTQzMzM4OTl9.FVKpFZ2VmRsbHzMkLMwIBTyO-VWztVUvHvirqWuURgM", description: "Email address" })
+    token: string
+}

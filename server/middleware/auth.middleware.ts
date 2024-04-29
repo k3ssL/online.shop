d56cb71e2e5d5db0605908c9d78ea205d3118d3a
k3ssL <viqwer3@yandex.ru>
2024-04-29 22:56:30 +0500

    create auth middleware

--- server/middleware/auth.middleware.ts
+++ server/middleware/auth.middleware.ts
@@ -3,37 +3,38 @@ import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 import {User} from "../src/users/users.model";
 import {JwtPayload} from "jsonwebtoken";
+import {UsersService} from "../src/users/users.service";
 
 
 export interface JwtResponseInterface extends JwtPayload {
-    id: number;
+    id: number
 }
 
 export interface ExpressRequestInterface extends Request {
-    user?: User;
+    user?: User
 }
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-    constructor(private readonly userRepository: typeof User) {}
+    constructor(private readonly usersService: UsersService) {}
 
     async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
         if (req.method === 'OPTIONS') {
-            next();
-            return;
+            next()
+            return
         }
 
         try {
             const token = req.headers.authorization.split(' ')[1];
             if (!token) {
-                return res.status(401).json({ message: 'Not authorized' });
+                return res.status(401).json({ message: 'Not authorized' }) 
             }
-            const decoded = jwt.verify(token, process.env.PRIVATE_KEY) as JwtResponseInterface;
-            const user = await this.userRepository.findOne({where: {id: decoded.id}})
-            req.user = user;
-            next();
+            const decoded = jwt.verify(token, process.env.PRIVATE_KEY) as JwtResponseInterface
+            const user = await this.usersService.findById(decoded.id);
+            req.user = user
+            next()
         } catch (e) {
-            res.status(401).json({ message: 'Not authorized' });
+            res.status(401).json({ message: 'Not authorized' })
         }
     }
 }

805c75a264311b3601e336d7fe2889e57645717e
k3ssL <viqwer3@yandex.ru>
2024-04-30 01:10:26 +0500

    decompose logic check role middleware

--- server/middleware/checkRole.middleware.ts
+++ server/middleware/checkRole.middleware.ts
@@ -1,43 +1,26 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import * as jwt from 'jsonwebtoken';
-import {User, UserRoleEnum} from "../src/users/users.model";
-import {JwtPayload} from "jsonwebtoken";
-import {UsersService} from "../src/users/users.service";
-
-
-export interface JwtResponseInterface extends JwtPayload {
-    id: number
-    role: string
-}
-
-export interface ExpressRequestInterface extends Request {
-    user?: User
-}
+import { Response, NextFunction } from 'express';
+import {ExpressRequestInterface} from "./auth.middleware";
+import {verifyToken} from "../utils/auth.utils";
 
 @Injectable()
 export class CheckRoleMiddleware implements NestMiddleware {
-    constructor(private readonly usersService: UsersService, private readonly role: UserRoleEnum) {}
     async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
         if (req.method === 'OPTIONS') {
             next()
             return
         }
 
-        try {
-            const token = req.headers.authorization.split(' ')[1];
-            if (!token) {
-                return res.status(401).json({ message: 'Not authorized' })
-            }
-            const decoded = jwt.verify(token, process.env.PRIVATE_KEY) as JwtResponseInterface
-            const user = await this.usersService.findById(decoded.id);
-            if (decoded.role !== this.role) {
-                return res.status(403).json({ message: 'Forbidden' })
-            }
-            req.user = user
-            next()
-        } catch (e) {
-            res.status(401).json({ message: 'Not authorized' })
+        const decoded = verifyToken(req);
+        if (!decoded) {
+            return res.status(401).json({ message: 'Not authorized' });
         }
+        console.log(decoded)
+        if (decoded.role !== 'ADMIN') {
+            return res.status(403).json({ message: 'Forbidden' })
+        }
+
+        req.user = decoded.user
+        next()
     }
 }

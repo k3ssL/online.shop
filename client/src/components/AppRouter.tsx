3995ce2730479727f9bfdb16cc3c981d92e1c6d4
k3ssL <viqwer3@yandex.ru>
2024-05-01 01:36:15 +0500

    config routers

---
+++ client/src/components/AppRouter.tsx
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import {Route, Routes, Navigate} from "react-router-dom";
+import {authRoutes, publicRoutes} from "../routes";
+import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
+
+const AppRouter = () => {
+    const {user} = useContext(Context)
+    return (
+        <Routes>
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+
+            {user.isAuth && authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component />} />
+            )}
+            <Route path="*" element={<Navigate to={SHOP_ROUTE} />} />A
+        </Routes>
+    );
+};
+
+export default AppRouter;

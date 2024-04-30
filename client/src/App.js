0a6a81201a59945bd3762265d90514783672542c
k3ssL <viqwer3@yandex.ru>
2024-04-30 23:33:05 +0500

    set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter"
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+      <div className="App">
+          <BrowserRouter>
+              <AppRouter/>
+          </BrowserRouter>
+      </div>
+  )
 }
 
 export default App;

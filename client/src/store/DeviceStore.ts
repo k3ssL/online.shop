90dfb155bd254e62b1da1ef4ba86a6fc4d8fda15
k3ssL <viqwer3@yandex.ru>
2024-05-11 00:38:00 +0500

    add mobX store

---
+++ client/src/store/DeviceStore.ts
@@ -0,0 +1,78 @@
+import {makeAutoObservable} from "mobx";
+import {TypeDto} from "../entities/Type/models/dto/TypeDto";
+import {BrandDto} from "../entities/Brand/models/dto/BrandDto";
+import {DeviceDto} from "../entities/Device/models/dto/DeviceDto";
+
+export default class DeviceStore {
+    private _types: TypeDto[]
+    private _brands: BrandDto[]
+    private _devices: DeviceDto[]
+    private _selectedType: TypeDto
+    private _selectedDevice: DeviceDto
+
+    constructor() {
+        this._types = [
+            {id: 1, name: 'Холодильники'},
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Ноутбуки'},
+            {id: 4, name: 'Телевизоры'},
+        ]
+
+        this._brands = [
+            {id: 1, name: 'Samsung'},
+            {id: 2, name: 'Apple'}
+        ]
+
+        this._devices = [
+            {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5, brandId: 1, typeId: 2, img: "https://cdn1.ozone.ru/s3/multimedia-n/6069408023.jpg"},
+            {id: 2, name: 'Iphone 12 pro', price: 25000, rating: 5, brandId: 1, typeId: 2, img: "https://cdn1.ozone.ru/s3/multimedia-n/6069408023.jpg"},
+            {id: 3, name: 'Iphone 12 pro', price: 25000, rating: 5, brandId: 1, typeId: 2, img: "https://cdn1.ozone.ru/s3/multimedia-n/6069408023.jpg"},
+            {id: 4, name: 'Iphone 12 pro', price: 25000, rating: 5, brandId: 1, typeId: 2, img: "https://cdn1.ozone.ru/s3/multimedia-n/6069408023.jpg"},
+        ]
+
+        this._selectedType = {} as TypeDto
+        this._selectedDevice = {} as DeviceDto
+
+        makeAutoObservable(this)
+    }
+
+    setTypes(types: TypeDto[]) {
+        this._types = types
+    }
+
+   setBrands(brands: BrandDto[]) {
+        this._brands = brands
+   }
+
+    setDevices(devices: DeviceDto[]) {
+        this._devices = devices
+    }
+
+    setSelectedType(type: TypeDto) {
+        this._selectedType = type
+    }
+
+    setSelectedDevice(device: DeviceDto) {
+        this._selectedDevice = device
+    }
+
+    get types() {
+        return this._types
+    }
+
+    get brands() {
+        return this._brands
+    }
+
+    get devices() {
+        return this._devices
+    }
+
+    get selectedType() {
+        return this._selectedType
+    }
+
+    get selectedDevice() {
+        return this._selectedDevice
+    }
+}
---
+++ client/src/store/UserStore.ts
@@ -0,0 +1,29 @@
+import {makeAutoObservable} from "mobx";
+import {UserDto} from "../entities/User/models/dto/UserDto";
+
+export default class UserStore {
+    private _isAuth: boolean
+    private _user: UserDto
+
+    constructor() {
+        this._isAuth = false
+        this._user = {} as UserDto
+        makeAutoObservable(this)
+    }
+
+    setIsAuth(bool: boolean) {
+        this._isAuth = bool
+    }
+
+    setUser(user: UserDto) {
+        this._user = user
+    }
+
+    get isAuth() {
+        return this._isAuth
+    }
+
+    get user() {
+        return this._user
+    }
+}

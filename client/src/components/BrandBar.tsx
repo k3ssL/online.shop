bdde9b3b8b0939d3aed2385f21a75b9d5b119f37
k3ssL <viqwer3@yandex.ru>
2024-05-11 01:00:13 +0500

    add brand bar component

---
+++ client/src/components/BrandBar.tsx
@@ -0,0 +1,26 @@
+import React, {useContext} from 'react';
+import {Card, Row} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {BrandDto} from "../entities/Brand/models/dto/BrandDto";
+
+const BrandBar = observer(() => {
+    const {device} = useContext(Context)
+
+    return (
+        <Row className='d-flex'>
+            {device.brands.map((brand: BrandDto) => (
+                <Card key={brand.id}
+                      className='p-3'
+                      onClick={() => device.setSelectedBrand(brand)}
+                      border={brand.id === device.selectedBrand.id ? 'danger' : 'light'}
+                      style={{cursor: 'pointer'}}
+                >
+                    {brand.name}
+                </Card>
+            ))}
+        </Row>
+    );
+});
+
+export default BrandBar;

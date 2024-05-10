bc4a1a7cb9e93225d3f2f27f4566edaa6bd325f0
k3ssL <viqwer3@yandex.ru>
2024-05-11 00:36:14 +0500

    add component type bar

---
+++ client/src/components/TypeBar.tsx
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {ListGroup} from "react-bootstrap";
+import {DeviceDto} from "../entities/Device/models/dto/DeviceDto";
+
+const TypeBar = observer(() => {
+    const {device} = useContext(Context)
+    return (
+        <ListGroup>
+            {device.types.map((type: DeviceDto) => (
+                <ListGroup.Item
+                    key={type.typeId}
+                    onClick={() => device.setSelectedType(type)}
+                    active={type.id === device.selectedType.id}
+                    style={{cursor: "pointer"}}
+                >
+                    {type.name}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    );
+});
+
+export default TypeBar;

b27e3191eebaa4946593fd3b137d7cd30bf1d266
k3ssL <viqwer3@yandex.ru>
2024-05-11 23:33:25 +0500

    add component device item

---
+++ client/src/components/DeviceItem.tsx
@@ -0,0 +1,32 @@
+import React, {FC} from 'react';
+import {Card, Col, Image} from 'react-bootstrap';
+import {DeviceDto} from "../entities/Device/models/dto/DeviceDto";
+import {useNavigate} from 'react-router-dom'
+import {DEVICE_ROUTE} from "../utils/consts";
+// @ts-ignore
+import star from "../assets/star.png";
+
+interface IDeviceItemProps {
+    device: DeviceDto
+}
+const DeviceItem: FC<IDeviceItemProps> = ({device}) => {
+    const navigate = useNavigate()
+
+    return (
+        <Col md={3} className='mt-3' onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
+            <Card style={{width: 150, cursor: 'pointer'}} border={'light'}>
+                <Image width={150} height={150} src={device.img}/>
+                <div className='text-black-50 mt-1 d-flex justify-content-between'>
+                    <div>Samsung</div>
+                    <div className='d-flex align-items-center'>
+                        <div>{device.rating}</div>
+                        <Image width={20} height={20} src={star}/>
+                    </div>
+                </div>
+                <div>{device.name}</div>
+            </Card>
+        </Col>
+    );
+};
+
+export default DeviceItem;

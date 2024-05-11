146e62b099c41bbdd0f0ed53e5095c475356d2c3
k3ssL <viqwer3@yandex.ru>
2024-05-12 00:45:01 +0500

    add modals for admin page and props interface

---
+++ client/src/components/modals/CreateBrand.tsx
@@ -0,0 +1,31 @@
+import React, {FC} from 'react';
+import {Button, Form, Modal} from "react-bootstrap";
+import {IModalProps} from "./IModalProps";
+
+const CreateBrand: FC<IModalProps> = ({show, onHide}) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить бренд
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control placeholder={'Введите название бренда'}/>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant={'outline-danger'} onClick={onHide}>Закрыть</Button>
+                <Button variant={'outline-success'} onClick={onHide}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CreateBrand;

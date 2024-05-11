511617125f0b8d505164fd78f70bcf135d12ee0b
k3ssL <viqwer3@yandex.ru>
2024-05-11 23:59:27 +0500

    routes

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useNavigate} from "react-router-dom";
 
 const NavBar = observer(() => {
+    const navigate = useNavigate()
+
     const {user} = useContext(Context)
     return (
         <Navbar bg="dark" data-bs-theme="dark">
@@ -13,8 +16,8 @@ const NavBar = observer(() => {
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>DNDS</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style = {{color: 'white'}}>
-                        <Button variant={'outline-light'}>Админ панель</Button>
-                        <Button variant={'outline-light'} style={{ marginLeft:'20px' }} onClick = {() => user.setIsAuth(false)}>Выйти</Button>
+                        <Button variant={'outline-light'} onClick={() => navigate(ADMIN_ROUTE)}>Админ панель</Button>
+                        <Button variant={'outline-light'} style={{ marginLeft:'20px' }} onClick={() => navigate(LOGIN_ROUTE)}>Выйти</Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style = {{color: 'white'}}>
